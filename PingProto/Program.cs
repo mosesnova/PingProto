245b29b45da87d88e7b8bd802a0c98e9e6bc379b
Moses Nova <moses@npl.nz>
2023-02-17 17:58:57 +1300

    Add project files.

---
+++ PingProto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.NetworkInformation; //Include this
+
+namespace PingProto
+{
+    public class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Ping myPing = new Ping();
+                PingReply reply = myPing.Send("192.168.1.3", 1000);
+                if (reply != null)
+                {
+
+                    Console.WriteLine("Status :  " + reply.Status + " \n Time : " + reply.RoundtripTime.ToString() + " \n Address : " + reply.Address);
+                    //Console.WriteLine(reply.ToString());
+                }
+            }
+            catch
+            {
+                Console.WriteLine("ERROR: You have Some TIMEOUT issue");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
